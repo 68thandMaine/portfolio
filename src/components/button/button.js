6b3d79fda3fb77e6adcf49415095f86cb389a93e
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-09-05 23:37:20 -0700

    add: Restyle the project view and add integration test structure

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import {Redirect} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './button.css';
 
 
 const Button = (props) => {
   return (
-    <button className={`button + ${props.purpose}`}>{props.text}</button>
+    <button className={`button + ${props.purpose}`}
+      onClick={props.clickEvent}>{props.text}</button>
   )
 }
 
 Button.propTypes = {
   text: PropTypes.string,
-  purpose: PropTypes.string
+  purpose: PropTypes.string,
+  clickEvent: PropTypes.func,
 }
 
 export default Button;
--- src/views/projects/Projects.js
+++ src/views/projects/Projects.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import './Projects.css';
@@ -6,81 +6,56 @@ import './Projects.css';
 import Card from '../../components/card/Card.js';
 import LanguageChart from '../../components/language-chart/LanguageChart.js';
 import LoadingBars from '../../components/loading-bar/LoadingBar';
+import PageHeader from '../../components/page-header/PageHeader';
 import RepoMenu from '../../components/repo-menu/RepoMenu.js';
 
-class Projects extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // projectToView: null,
-    };
-    this.viewToShow = this.viewToShow.bind(this);
-  }
+function Projects(props) {
+  useEffect(() => {
+    viewToShow();
+  });
 
-  componentWillUpdate(){
-    this.viewToShow();
-  }
-
-  componentDidMount(){
-    this.viewToShow();
-  }
-
-  viewToShow() {
-    let repositories = Object.entries(this.props.repositories);
-
+  function viewToShow() {
+    let repositories = Object.entries(props.repositories);
     if(repositories.length === 0) {
       return <LoadingBars />
     } else 
-    return  <LanguageChart repositoryList={this.props.repositories} />
+    return  <LanguageChart repositoryList={props.repositories} />
   }
 
-  render() {
-
-    
-
-    return (
-      <div className='project-wrapper'>
-        <div className='project-header'>
-          <h1>Always Building</h1>
+  return (
+    <div className='project-wrapper'>
+      <div className='project-header'>
+        <PageHeader
+        title='ALWAYS BUILDING'
+        />
+      </div>
+      <div className='project-body'>
+        <div className='body-menu-panel'>
+          <div className='text-container'>
+            <p>I love to code. There\'s just something about the logical creativity needed to create web applications that excites me.</p>
+            <br />
+            <p>Here you can find the body of my work from 2018 to present.</p>
+            <br />
+            <p>I primarily write in C# or JavaScript, but I have experience working with some PHP as well.</p>
+            <br />
+            <p>My projects are tailored to the specific needs of each client, and I enjoy the collaborative effort that brings code to life.</p>
+            <p>Essentially I design, build, and maintain meaningful web applications that are easy to use.</p>
+          </div>
+          {viewToShow()}
         </div>
-     
-        <div className='project-body'>
-          <div className='body-menu-panel'>
-            
-            <div className='text-container'>
-              <p>I love to code. There's just something about the logical creativity needed to create web applications that excites me.</p>
-              <br />
-              <p>Here you can find the body of my work from 2018 to present.</p>
-              <br />
-              <p>I primarily write in C# or JavaScript, but I have experience working with some PHP as well.</p>
-              <br />
-              <p>My projects are tailored to the specific needs of each client, and I enjoy the collaborative effort that brings code to life.</p>
-              <p>Essentially I design, build, and maintain meaningful web applications that are easy to use.</p>
-            </div>
-            
-            {this.viewToShow()}
-           
-            
-
-          </div>
-
-
-
-          <div className='body-project-panel'>
-    
-            <Card 
-              repositoryList={this.props.personalProjects}  
-            />
-
-          <RepoMenu 
-            repositoryList={this.props.repositories} 
+        <div className='body-project-panel'>
+          <Card 
+            repositoryList={props.personalProjects}  
           />
-            
-          </div>
+
+        <RepoMenu 
+          repositoryList={props.repositories} 
+        />s
+          
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 Projects.propTypes = {
