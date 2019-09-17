cee448ed550b5041ec55f147cfb9970e37b844ed
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-09-16 18:46:26 -0700

    update: remove unecessary file

--- src/assets/images/images.js
+++ src/assets/images/images.js
@@ -1,7 +1,7 @@
 import AboutBG  from './JPEG/AboutBG.jpg';
 import HomeBG from './JPEG/HomeBG.jpg';
-import ProjectBG from './JPEG/ProjectBG.jpg'
-import NotFoundBG from './notfound.png';
+import ProjectBG from './JPEG/ProjectBG.jpg';
+import NotFoundBG from './JPEG/notfound.jpg';
 
 export  const images = [
   HomeBG,
