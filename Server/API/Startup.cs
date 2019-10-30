f569c1debd7603e4ad795b78bd703bca9d91a8fe
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-10-29 23:03:21 -0700

    Add Logger Service

--- Server/API/Startup.cs
+++ Server/API/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace server
+namespace API
 {
     public class Startup
     {
@@ -25,10 +25,10 @@ namespace server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.ConfigureCors();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.x
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
