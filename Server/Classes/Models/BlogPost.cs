e748bd82605eb008facad5f9a248003d6c1f5b14
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-10-30 00:32:24 -0700

    Add BlogPost model

---
+++ Server/Classes/Models/BlogPost.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Backend.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string BlogTitle { get; set; }
+        public string WrittenOn { get; set; }
+        public string BlogContent { get; set; }
+        public string[] Categories { get; set; }
+    }
+}
