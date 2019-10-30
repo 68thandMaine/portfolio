c69f9669f368a6139e3b02b0978d1937c7f7bfe8
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-10-30 00:12:13 -0700

    ADD Custom Configuration for Dependency Injection Services

---
+++ Server/API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Services;
+using Interfaces;
+
+namespace API.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .WithMethods("GET", "PUT", "POST", "DELETE")
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+        }
+        public static void ConfigureLoggerService(this IServiceCollection services)
+        {
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+        }
+
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+        }
+    }
+}
