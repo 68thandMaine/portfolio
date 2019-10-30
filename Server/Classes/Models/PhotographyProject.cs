0bbde64619540ba90f9730a34191525dda4b53a4
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-10-30 00:32:42 -0700

    Add PhotographyProject Model

---
+++ Server/Classes/Models/PhotographyProject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace Backend.Models
+{
+    public class PhotographyProject
+    {
+        public int Id { get; set; }
+        public string PhotoName { get; set; }
+        public string ImgURL { get; set; }
+        public string Description { get; set; }
+        public string CreatedAt { get; set; }
+        public string Category { get; set; }
+    }
+}
