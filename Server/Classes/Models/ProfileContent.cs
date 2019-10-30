ba259880f51f971aaaf25e9800cf0b16a1fee861
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-10-30 00:33:09 -0700

    Add ProfileContent Model

---
+++ Server/Classes/Models/ProfileContent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Backend.Models
+{
+    public class ProfileContent
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public long Resume { get; set; }
+        public string Bio { get; set; }
+        public string[] PageContent { get; set; }
+    }
+}
